67ee69bbba445a424427d2c0d1c288d4e0521d8d
WuanBui <118819560+WuanBui@users.noreply.github.com>
2023-10-27 15:11:00 +0700

    Practice 2

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('v1')->group(function () {
+    Route::apiResource('categories', \App\Http\Controllers\API\CategoryController::class);
+    Route::apiResource('products', \App\Http\Controllers\API\ProductController::class);
+});
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
